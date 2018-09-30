74ecd53fd69bf240f73e84c0da6d38f28f5d2135
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-09-30 10:19:50 +0200

    Remove servers component from app component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,11 @@
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
-
-
+    serverId = Math.round(Math.random() * 100);
+    serverStatus = (this.serverId % 2 === 0) ? 'online' : 'offline';
 
 }
