92749c36779437361b91ca90427f51613ff7994a
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-09-30 10:59:31 +0200

    Add Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
