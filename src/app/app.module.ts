8babe36c4f98df80781f29d247b5521449b9ca99
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-09-30 11:00:59 +0200

    Create Recipes Component
    ng g c recipes --spec false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
