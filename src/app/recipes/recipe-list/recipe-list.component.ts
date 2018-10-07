4ed1496e39eb5e66f685f08a460c5c65ab46d2af
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-10-07 16:39:27 +0200

    Recipe listing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+
+    recipes: Recipe[] = [
+       this.recipeCreate(),
+       this.recipeCreate()
+    ];
+
+    public recipeCreate() {
+        const name = 'Udon Noodle Soup';
+        const desc = 'Fast food Japanese style! Start to finish, this healthy and light Japanese Udon Noodle Soup';
+        const imgPath = 'https://steamykitchen.com/wp-content/uploads/2010/07/udon-miso-noodle-soup-2805.jpg';
+        return  new Recipe(name, desc, imgPath);
+    }
+
+  constructor() {
+
+
+
+  }
 
   ngOnInit() {
   }
